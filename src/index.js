6c5fbc64efd9cd1e0d06226b66f57fb79260baaa
Andrey Linko <AndreyLinko@gmail.com>
2015-10-14 09:27:19 +0000

    Themes

--- src/index.js
+++ src/index.js
@@ -7,22 +7,25 @@ export default function style(classMap) {
   return (target, key, descriptor) => {
     const classNameGetter = `get${key.replace('render', '')}ClassName`
 
-    target[classNameGetter] = function getClassName() {
-      const superMethod = babelHelpers.get(Object.getPrototypeOf(target.constructor.prototype), classNameGetter, this)
-      const classNames = []
+    Object.defineProperty(target.constructor.prototype, classNameGetter, {
+      configurable: true,
+      value: function getClassName() {
+        const superMethod = babelHelpers.get(Object.getPrototypeOf(target.constructor.prototype), classNameGetter, this)
+        const classNames = []
 
-      if (superMethod) {
-        classNames.push(superMethod.call(this))
-      }
-
-      for (const modifier in classMap) {
-        if (modifierMatch(modifier, this.props, this.state)) {
-          classNames.push(classMap[modifier])
+        if (superMethod) {
+          classNames.push(superMethod.call(this))
         }
-      }
 
-      return classNames.join(' ')
-    }
+        for (const modifier in classMap) {
+          if (modifierMatch(modifier, this.props, this.state)) {
+            classNames.push(classMap[modifier])
+          }
+        }
+
+        return classNames.join(' ')
+      },
+    })
 
     return descriptor
   }
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import {
+  is,
   head,
   tail,
   map,
@@ -7,10 +8,15 @@ import {
   converge,
   join,
   pathEq,
+  path,
   init,
   last,
   split,
   flatten,
+  match,
+  replace,
+  toLower,
+  isEmpty,
 } from 'ramda'
 
 export const capitalize = converge((h, t) => h + t, pipe(toUpper, head), tail)
@@ -35,3 +41,19 @@ export const modifierMatch = (modifier, props = {}, state = {}) => {
 
   return matcher(props) || matcher(state)
 }
+
+export const isThemeVariable = value => is(String, value) && !isEmpty(match(/\$/, value))
+
+export const themeVariableToPath = pipe(tail, split('.'))
+
+export const getValueFromTheme = (value, theme) => {
+  if (head(value) !== '$') return value
+  const result = path(themeVariableToPath(value), theme)
+  return result && result.toString ? result.toString() : result
+}
+
+export const injectThemeVariables = (value, theme) => {
+  return pipe(split(' '), map(item => getValueFromTheme(item, theme)), join(' '))(value)
+}
+
+export const modifierToAttribute = pipe(replace(/([A-Z])/g, '-$1'), toLower)
